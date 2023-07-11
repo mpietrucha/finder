370ee63d074c23f43f110728054232db76f3e8c5
Michał Pietrucha <michal.pietrucha@off-site.pl>
2023-07-11 14:43:30 +0200

    wip

--- src/Skeleton.php
+++ src/Skeleton.php
@@ -5,8 +5,8 @@ namespace Mpietrucha\Finder;
 use Mpietrucha\Support\Macro;
 use Illuminate\Support\Collection;
 use Illuminate\Support\LazyCollection;
-use Mpietrucha\Exception\RuntimeException;
 use Mpietrucha\Support\Concerns\HasFactory;
+use Mpietrucha\Exception\BadMethodCallException;
 use Mpietrucha\Support\Concerns\InteractsWithTime;
 
 abstract class Skeleton
@@ -22,11 +22,11 @@ abstract class Skeleton
 
     public function find(): Collection
     {
-        throw new RuntimeException('Method', ['find'], 'is not allowed in this finder instance');
+        throw new BadMethodCallException('Method', ['find'], 'is not allowed in this finder instance');
     }
 
     public function lazy(): LazyCollection
     {
-        throw new RuntimeException('Method', ['lazy'], 'is not allowed in this finder instance');
+        throw new BadMethodCallException('Method', ['lazy'], 'is not allowed in this finder instance');
     }
 }
